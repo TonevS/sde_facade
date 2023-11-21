1cac3689d76e9ee8758b59c538996be40e6de31c
t0nkisa <antanitonev03@gmail.com>
2023-11-21 16:02:28 +0100

    RadioListenerFacade done

--- src/app/RadioListenerFacade.java
+++ src/app/RadioListenerFacade.java
@@ -1,10 +1,22 @@
 package app;
 
 public class RadioListenerFacade implements Process{
+    private final Amplifier amplifier;
+    private final Tuner tuner;
+    public RadioListenerFacade() {
+        amplifier = new Amplifier("Top-O-Line Amplifier");
+        tuner = new Tuner("Top-O-Line AM/FM Tuner", amplifier);
+    }
     public void start() {
+        tuner.on();
+        tuner.setFrequency(44.63);
 
+        amplifier.on();
+        amplifier.setVolume(5);
+        amplifier.setTuner(tuner);
     }
     public void stop() {
-
+        tuner.off();
+        amplifier.off();
     }
 }
