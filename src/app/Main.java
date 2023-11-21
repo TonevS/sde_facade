cdcba5aa5d6beb37e9b9c2c48272164f31d7c7c1
t0nkisa <antanitonev03@gmail.com>
2023-11-21 16:10:54 +0100

    Added more lines for readability

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,13 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         MovieWatcherFacade movieWatcher = new MovieWatcherFacade();
+        System.out.println("Watching movies!");
         movieWatcher.start();
+        System.out.println("Finished watching!");
         movieWatcher.stop();
+        System.out.println();
+
         MusicListenerFacade musicListener = new MusicListenerFacade();
+        System.out.println("Listening to music!");
         musicListener.start();
+        System.out.println("Finished listening to music");
         musicListener.stop();
+        System.out.println();
+
         RadioListenerFacade radioListener = new RadioListenerFacade();
+        System.out.println("Listening to radio!");
         radioListener.start();
+        System.out.println("Finished listening to music");
         radioListener.stop();
     }
 }
