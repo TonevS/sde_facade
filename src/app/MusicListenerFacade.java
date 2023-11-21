2ccc2adb21041f2b721144a34607bc5d907cc9e0
t0nkisa <antanitonev03@gmail.com>
2023-11-21 15:58:14 +0100

    MusicListenerFacade done

--- src/app/MusicListenerFacade.java
+++ src/app/MusicListenerFacade.java
@@ -1,10 +1,32 @@
 package app;
 
 public class MusicListenerFacade implements Process{
-    public void start() {
+    private final TheaterLights theaterLights;
+    private final Amplifier amplifier;
+    private final CdPlayer cdPlayer;
 
+    public MusicListenerFacade() {
+        theaterLights = new TheaterLights("Theater Ceiling Lights");
+        amplifier = new Amplifier("Top-O-Line Amplifier");
+        cdPlayer = new CdPlayer("Top-O-Line CD Player", amplifier);
+    }
+
+    public void start() {
+        theaterLights.on();
+
+        amplifier.on();
+        amplifier.setVolume(5);
+        amplifier.setCd(cdPlayer);
+        amplifier.setStereoSound();
+
+        cdPlayer.on();
+        cdPlayer.play(4);
     }
     public void stop() {
+        amplifier.off();
+        amplifier.setCd(cdPlayer);
 
+        cdPlayer.eject();
+        cdPlayer.off();
     }
 }
