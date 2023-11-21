0d4ae30acb91602d4674723e08e68ad3e2fe870c
t0nkisa <antanitonev03@gmail.com>
2023-11-21 15:34:10 +0100

    MovieWatcherFacade method start done

--- src/app/MovieWatcherFacade.java
+++ src/app/MovieWatcherFacade.java
@@ -2,7 +2,30 @@ package app;
 
 public class MovieWatcherFacade implements Process {
     public void start() {
+        PopcornPopper popcornPopper = new PopcornPopper("Popcorn Popper");
+        TheaterLights theaterLights = new TheaterLights("Theater Ceiling Lights");
+        Screen screen = new Screen("Theater Screen");
+        Amplifier amplifier = new Amplifier("Top-O-Line Amplifier");
+        DvdPlayer dvdPlayer = new DvdPlayer("Top-O-Line DVD Player", amplifier);
+        Projector projector = new Projector("Top-O-Line Projector", dvdPlayer);
 
+        popcornPopper.on();
+        popcornPopper.pop();
+
+        theaterLights.dim(4);
+
+        screen.down();
+
+        projector.on();
+        projector.wideScreenMode();
+
+        amplifier.on();
+        amplifier.setDvd(dvdPlayer);
+        amplifier.setSurroundSound();
+        amplifier.setVolume(5);
+
+        dvdPlayer.on();;
+        dvdPlayer.play("Django Unchained");
     }
     public void stop() {
 
